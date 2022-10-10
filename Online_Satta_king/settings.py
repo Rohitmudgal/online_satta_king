e219730680de0ff9acc5858b510f3831ad4c08ca
Rohitmudgal <111386313+Rohitmudgal@users.noreply.github.com>
2022-10-10 16:42:43 +0530

    Update settings.py

--- Online_Satta_king/settings.py
+++ Online_Satta_king/settings.py
@@ -78,10 +78,12 @@ WSGI_APPLICATION = 'Online_Satta_king.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD': 'root'
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT':5432
     }
 }
 
